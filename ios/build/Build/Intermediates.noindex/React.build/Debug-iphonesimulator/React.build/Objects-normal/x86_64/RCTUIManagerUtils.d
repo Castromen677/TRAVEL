abfc2fa14b82dc98a4756e35b160142c61770099
Babenkov Balya <castromen677@gmail.com>
2018-12-04 13:38:02 +0300

    Bug State

--- ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/React.build/Objects-normal/x86_64/RCTUIManagerUtils.d
+++ ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/React.build/Objects-normal/x86_64/RCTUIManagerUtils.d
@@ -1,5 +1,5 @@
 dependencies: \
-  /Users/castromen/Youhobby/node_modules/react-native/React/Modules/RCTUIManagerUtils.m \
-  /Users/castromen/Youhobby/node_modules/react-native/React/Modules/RCTUIManagerUtils.h \
-  /Users/castromen/Youhobby/node_modules/react-native/React/Base/RCTAssert.h \
-  /Users/castromen/Youhobby/node_modules/react-native/React/Base/RCTDefines.h
+  /Users/castromen/Downloads/TRAVEL/node_modules/react-native/React/Modules/RCTUIManagerUtils.m \
+  /Users/castromen/Downloads/TRAVEL/node_modules/react-native/React/Modules/RCTUIManagerUtils.h \
+  /Users/castromen/Downloads/TRAVEL/node_modules/react-native/React/Base/RCTAssert.h \
+  /Users/castromen/Downloads/TRAVEL/node_modules/react-native/React/Base/RCTDefines.h
---
+++ src/screen1/HomeScreen.js
@@ -0,0 +1,57 @@
+//Родитель
+import React, {Component} from 'react';
+import {StyleSheet, View, ScrollView} from 'react-native';
+import {Header, ImageCart} from "../components/uikit";
+
+const url = 'https://raw.githubusercontent.com/react-native-village/react-native-init/master/stargate/stargate.json'
+//const url = 'https://api.tvmaze.com/search/shows?q=girls'
+
+export default class HomeScreen extends Component {
+    //Model
+    state = {
+        title: 'TravelGuide',
+        data: []
+    }
+    componentDidMount = async () => {
+        try {
+            const respons = await fetch(url) // Get Data
+            const data = await respons.json()
+            this.setState({data}) //Post Date
+        } catch (e) {
+            throw e
+        }
+    }
+
+    //View
+    render() {
+        const {title, data} = this.state
+        const {containter, blockCart} = styles
+        return (
+            <View style={containter}>
+                <Header title={title}/>
+                <ScrollView>
+                    <View style={blockCart}>
+                        {data.map(item => (
+                            <ImageCart data={item} key={item.id}/>
+                        ))}
+                    </View>
+                </ScrollView>
+            </View>
+        )
+    }
+};
+
+const styles = StyleSheet.create({
+    containter: {
+        flex: 1,
+    },
+    blockCart: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        marginBottom: 120,
+        marginTop: 20
+    }
+
+})
+
